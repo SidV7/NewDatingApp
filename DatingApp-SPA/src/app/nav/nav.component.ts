accf6c8f546901c86076f96753bab25dd54ffdf6
Siddharth Vaikunthe <siddsv7@gmail.com>
2020-05-31 15:43:59 +0530

    End of section 7

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,38 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authservice: AuthService, private alertify: AlertifyService) { }
+  constructor(
+    public authservice: AuthService,
+    private alertify: AlertifyService,
+    private router: Router
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  login() {
+    this.authservice.login(this.model).subscribe(
+      (next) => {
+        this.alertify.success('Logged in successfully');
+      },
+      (error) => {
+        this.alertify.error(error);
+      },
+      () => {
+        this.router.navigate(['/members']);
+      }
+    );
   }
 
-  login(){
-    this.authservice.login(this.model).subscribe(next => {
-      this.alertify.success("Logged in successfully");
-    }, error => {
-      this.alertify.error(error);
-    });
-  }
-
-  loggedIn(){
+  loggedIn() {
     // const token = localStorage.getItem('token');
     // !! is short hand for if statement which would return true /false
     // return !!token;
     return this.authservice.loggedIn();
   }
 
-  logOut(){
+  logOut() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
-
 }
